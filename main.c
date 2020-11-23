47ba0eb748c17b49248e704820c754d6739d540c
soyounglee026 <71009039+soyounglee026@users.noreply.github.com>
2020-11-23 22:54:15 +0900

    12-6

--- main.c
+++ main.c
@@ -8,20 +8,17 @@ int main(int argc, char *argv[]) {
 	
 	FILE *fp;
 	char str[100];
-	int i;
 	
-	fp = fopen("sample.txt", "w");
+	fp = fopen("sample.txt", "r");
 	if(fp == NULL)
 	{
 		return -1;
 	}
 	
-	for(i=0;i<3;i++)
-	{
-		printf("Input a word : ");
-		scanf("%s", str);
-		fprintf(fp, "%s\n", str);
+	while(fgets(str, 100, fp) != NULL) {
+		printf("%s", str);
 	}
+	
 	fclose(fp);
 	
 	return 0;
